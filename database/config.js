ec874a23ce80e7b601ee344e440f661aec522750
MaulDeveloper <maulanafatahilahganteng@gmail.com>
2025-11-30 02:12:59 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8515728904:AAEhd3dYsgxP9qMJIYu1M8E7QLtkAkgHWKs",  // Masukin Bot token kamu
+  owners: "8275181483", // Masukin ID Telegram kamu
+  port: "2059", // Masukin Port panel kamu 
+  ipvps: "https://sellerxenn.serverteach.my.id" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
